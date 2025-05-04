97f1d8907f5b47acb02e74c4aea0d59c03c2e30a
che424 <claire.ely42@gmail.com>
2025-05-04 15:31:30 -0600

    Add files via upload

--- alg2.py
+++ alg2.py
@@ -9,40 +9,43 @@ def driver():
     roots = rootfind_alg2(an, bn, cn)
     print(roots)
 
+    xeval = np.linspace(-2, 2, 100)  # vector of x
+    P = cheb(n+1, xeval)
+    plt.figure()
+    plt.plot(xeval,P[n+1],label = f'P_{n}')
+    plt.title(f'P_{n}')
+    for root in roots:
+        plt.scatter(root, y= 0,  color = 'black', s = 50, marker='x', label = 'x = ' + str(root))
+    plt.legend()
+    plt.axhline(y=0, color='k')
+    plt.axvline(x=0, color='k')
+    plt.show()
+
+
 def rootfind_alg2(an, bn, cn):
-    ##Takes recurrence relation coefficients
     n = len(an)
-
-    # Step 1: Compute diags of jacobi 
     alpha = np.array([-bn[i] / an[i] for i in range(n)])
-
-    # Step 2: Compute off diag of jacobi
     beta = np.array([np.sqrt(cn[i+1] / (an[i] * an[i+1])) for i in range(n - 1)])
-
-    # Step 3: build jacobi
     J = np.zeros((n, n))
     np.fill_diagonal(J, alpha)
     for i in range(n - 1):
         J[i, i + 1] = beta[i]
         J[i + 1, i] = beta[i]
     print(J)
-    # Step 4: QR Algorithm
-    max_iter = 1000
-    tol = 1e-12
-    J = QR(J)
-
-    # Step 5: Roots are the eigenvalues (diagonal of converged J)
-    return np.sort(np.diag(J))
+    eigvals = np.linalg.eigvalsh(J)
+    return np.sort(eigvals)
 
 
-def generate_tchebycoeffs(n): #generates array of length n with tcheby 3 term coeffs
+def generate_tchebycoeffs(n):
+    # For Chebyshev polynomials of the first kind
     an = np.ones(n)*2
-    an[0] = 1
-    bn = np.zeros(n)
-    cn = np.ones(n)
+    an[0] = 1   
+    bn = np.zeros(n)         
+    cn = np.ones(n)           
     return an, bn, cn
 
-def QR(A, max_iter, tol):
+
+def QR(J):
     max_iter = 1000
     tol = 1e-12
     for _ in range(max_iter):
@@ -53,5 +56,15 @@ def QR(A, max_iter, tol):
         J = J_new
     return J
 
+
+def cheb(n, xeval):
+    T = np.zeros((n+1, len(xeval)))  # 2d array holds n+1 arrays of xevals
+    T[0, :] = 1  # T0(x) = 1
+    if n > 0:
+        T[1, :] = xeval  # T1(x) = x
+    
+    for i in range(2, n+1): # applying recurrence relation
+        T[i, :] = 2 * xeval * T[i-1, :] - T[i-2, :]
+    return T
 
 driver()
